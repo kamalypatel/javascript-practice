f0526a34a2b0eabb76849f455294b2f74f50179a
Kamal Patel <80398694+kamalypatel@users.noreply.github.com>
2024-03-10 15:17:18 -0400

    added input box clearing and cleaned up html

--- simple-shopping-list/scripts.js
+++ simple-shopping-list/scripts.js
@@ -1,31 +1,32 @@
 const button = document.querySelector('#submit');
 
-const list = document.querySelector('ul')
+const list = document.querySelector('ul');
 
-let i = 0
+let i = 0;
 
 button.addEventListener('click', () => {
     let listItem = document.getElementById('name').value;
+    document.getElementById('name').value = '';
     
-    const itemOnList = document.createElement('li')
-    itemOnList.textContent = listItem
-    itemOnList.setAttribute('id', i)
-    itemOnList.setAttribute('class', 'listItems')
+    const itemOnList = document.createElement('li');
+    itemOnList.textContent = listItem;
+    itemOnList.setAttribute('id', i);
+    itemOnList.setAttribute('class', 'listItems');
 
     const deleteButton = document.createElement('button');
-    deleteButton.setAttribute('type', 'button')
-    deleteButton.setAttribute('id', i)
+    deleteButton.setAttribute('type', 'button');
+    deleteButton.setAttribute('id', i);
     deleteButton.setAttribute('class', 'listItems')
-    deleteButton.textContent = 'Delete'
-    i++
+    deleteButton.textContent = 'Delete';
+    i++;
 
     deleteButton.addEventListener('click', function() {
         itemOnList.remove();
         this.remove();
     })
 
-    itemOnList.appendChild(deleteButton)
-    list.appendChild(itemOnList)
+    itemOnList.appendChild(deleteButton);
+    list.appendChild(itemOnList);
 
     
 });
