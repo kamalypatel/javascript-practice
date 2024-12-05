f8b305d9c1886fdc46e3bb48241f801d2e70d5bb
Kamal Patel <80398694+kamalypatel@users.noreply.github.com>
2024-12-05 07:58:01 -0500

    got the fibs regular working

--- recursion/scripts2.js
+++ recursion/scripts2.js
@@ -2,18 +2,33 @@
 
 // fibonacci regular
 
+// let n = 10
+// let sum = []
+// for (let i = 0; i < n; i++){
+//     if (i == 0) {
+//         sum.push(0)
+//         console.log(sum)
+//     } else if (i == 1) {
+//         sum.push(1)
+//         console.log(sum)
+//     } else {
+//         sum[i] = sum[i-1] + sum[i-2]
+//         console.log(sum)
+//     }
+// }
+
+
 // fibonacci recursive
 
+// function fibs(n) {
+//     if (n == 0) {
+//         return 0
+//     } if (n==1) {
+//         return 1
+//     } else {
+//         return fibs(n-2) + fibs(n-1)
+//     }
+// }
 
-function fibs(n) {
-    if (n == 0) {
-        return 0
-    } if (n==1) {
-        return 1
-    } else {
-        return fibs(n-2) + fibs(n-1)
-    }
-}
-
-let n = 10
-console.log(fibs(n))
+// let n = 10
+// console.log(fibs(n))
