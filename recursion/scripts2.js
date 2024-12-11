bf966050d76b0ec5a8c94c5e79cc5b8286554335
Kamal Patel <80398694+kamalypatel@users.noreply.github.com>
2024-12-11 07:36:52 -0500

    started working on mergeSort

--- recursion/scripts2.js
+++ recursion/scripts2.js
@@ -32,3 +32,26 @@
 
 // let n = 9
 // console.log(fibs(n))
+
+
+// Merge Sort
+
+let array = [3, 2, 1, 13, 8, 5, 0, 1]
+let leftHalf = []
+let rightHalf = []
+
+function mergeSort(n){
+    let mid = n.length / 2
+
+    for (let i = 0; i < mid; i++){
+        leftHalf.push(n[i])
+    }
+    for (let i = mid; i < n.length; i++){
+        rightHalf.push(n[i])
+    }
+
+    console.log(leftHalf)
+    console.log(rightHalf)
+}
+
+mergeSort(array)
